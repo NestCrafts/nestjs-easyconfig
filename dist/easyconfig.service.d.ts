cdbcd9331cfc702856dad48aaed45d3772cce40d
kronos <roobin.bhandari@gmail.com>
2019-09-07 19:55:43 +0545

    fixed a bug on safe checking and removed debug with logger

--- dist/easyconfig.service.d.ts
+++ dist/easyconfig.service.d.ts
@@ -1,11 +1,10 @@
 import { Config } from './config.interface';
-import * as debug from 'debug';
 export declare class EasyconfigService {
     readonly envConfig: {
         [key: string]: string;
     };
-    readonly errorLog: debug.Debugger;
     readonly sampleFile: string;
+    private readonly logger;
     constructor(config?: Config);
     get(key: string): any;
     safeCheck: (userEnvFile: any, config: any) => void;
--- dist/easyconfig.service.js
+++ dist/easyconfig.service.js
@@ -3,26 +3,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv");
 const fs = require("fs");
 const path = require("path");
-const debug = require("debug");
+const common_1 = require("@nestjs/common");
 class EasyconfigService {
     constructor(config) {
-        this.errorLog = debug('warning');
         this.sampleFile = '.env.sample';
+        this.logger = new common_1.Logger(EasyconfigService.name);
         this.safeCheck = (userEnvFile, config) => {
             const src = Object.keys(dotenv.parse(fs.readFileSync(path.resolve(config))));
             const missingKeys = src
                 .filter(x => !userEnvFile.includes(x))
                 .concat(userEnvFile.filter(x => !src.includes(x)));
-            this
-                .errorLog(`MissingEnvVarsError: ${missingKeys} were defined in .env.example but are not present in the environment:
+            if (missingKeys.length !== 0) {
+                this.logger.error(`MissingEnvVarsError: ${missingKeys} were defined in .env.example but are not present in the environment:
         This may cause the app to misbehave.`);
+            }
+            else {
+                this.logger.debug('Config looks good :) ');
+            }
         };
-        debug.enable('warning');
         if (!config.path && process.env.NODE_ENV) {
             this.envConfig = dotenv.parse(fs.readFileSync(path.resolve(`.env.${process.env.NODE_ENV}`)));
         }
         else if (!config.path && !process.env.NODE_ENV) {
-            this.errorLog('Failed to load configs. Either pass file or NODE_ENV :(');
+            this.logger.error('Failed to load configs. Either pass file or NODE_ENV :(');
             return;
         }
         else {
@@ -35,7 +38,7 @@ class EasyconfigService {
     get(key) {
         const val = this.envConfig[key];
         if (!val) {
-            this.errorLog('The key was not found in config file :(');
+            this.logger.warn('The key was not found in config file :(');
             return;
         }
         if (/^\d+$/.test(val)) {
