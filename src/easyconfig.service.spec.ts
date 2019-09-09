c268d63dfbb7757f2ace12f31fd2c6ea89148d9c
kronos <roobin.bhandari@gmail.com>
2019-09-09 07:40:35 +0545

    fixed a typo

--- src/easyconfig.service.spec.ts
+++ src/easyconfig.service.spec.ts
@@ -1,18 +1,40 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { EasyconfigService } from './easyconfig.service';
 
 describe('EasyconfigService', () => {
-  let service: EasyconfigService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [EasyconfigService],
-    }).compile();
-
-    service = module.get<EasyconfigService>(EasyconfigService);
-  });
+  const service: EasyconfigService = new EasyconfigService({ path: '.env.dev', safe: true });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should be return int', () => {
+    expect(service.get('KEYINT')).toEqual(100);
+  });
+
+  it('should be return boolean', () => {
+    expect(service.get('KEYBOOL')).toEqual(true);
+  });
+
+  it('should be return string', () => {
+    expect(service.get('KEYSTR')).toEqual('hello');
+  });
+});
+
+describe('EasyconfigService with NODE_ENV', () => {
+  process.env.NODE_ENV = 'dev';
+  const service: EasyconfigService = new EasyconfigService({});
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+});
+
+describe('EasyconfigService without NODE_ENV', () => {
+  const service: EasyconfigService = new EasyconfigService({});
+
+  it('should not be defined', () => {
+    expect(service).toBeDefined();
+  });
+
 });
