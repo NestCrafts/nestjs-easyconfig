7ae4707b2af9cbb566a0a4c2872ce824c856d08b
rubiin <roobin.bhandari@gmail.com>
2020-06-15 20:20:50 +0545

    chores

--- dist/easyconfig.module.js
+++ dist/easyconfig.module.js
@@ -5,29 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var EasyconfigModule_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.EasyconfigModule = void 0;
 const common_1 = require("@nestjs/common");
 const easyconfig_service_1 = require("./easyconfig.service");
-let EasyconfigModule = (() => {
-    var EasyconfigModule_1;
-    let EasyconfigModule = EasyconfigModule_1 = class EasyconfigModule {
-        static register(options) {
-            return {
-                module: EasyconfigModule_1,
-                providers: [
-                    {
-                        provide: easyconfig_service_1.EasyconfigService,
-                        useValue: new easyconfig_service_1.EasyconfigService(options),
-                    },
-                ],
-                exports: [easyconfig_service_1.EasyconfigService],
-            };
-        }
-    };
-    EasyconfigModule = EasyconfigModule_1 = __decorate([
-        common_1.Module({})
-    ], EasyconfigModule);
-    return EasyconfigModule;
-})();
+let EasyconfigModule = EasyconfigModule_1 = class EasyconfigModule {
+    static register(options) {
+        return {
+            module: EasyconfigModule_1,
+            providers: [
+                {
+                    provide: easyconfig_service_1.EasyconfigService,
+                    useValue: new easyconfig_service_1.EasyconfigService(options),
+                },
+            ],
+            exports: [easyconfig_service_1.EasyconfigService],
+        };
+    }
+};
+EasyconfigModule = EasyconfigModule_1 = __decorate([
+    common_1.Module({})
+], EasyconfigModule);
 exports.EasyconfigModule = EasyconfigModule;
