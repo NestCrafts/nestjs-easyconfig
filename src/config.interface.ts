225f1b1d7052c959ce14bfc06b4f0d744adf3270
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-10-01 15:01:30 +0545

    fix: move eslint to flat config

--- src/config.interface.ts
+++ src/config.interface.ts
@@ -1,79 +1,78 @@
-import { LoggerService } from '@nestjs/common';
-import { DotenvConfigOptions } from 'dotenv';
+import type { LoggerService } from "@nestjs/common";
+import type { DotenvConfigOptions } from "dotenv";
 
 export interface Config extends DotenvConfigOptions {
-	/**
-	 *
-	 * path of the file to check the keys against when safe option is set to true.
-	 * Defaults to .env.sample
-	 *
-	 */
+  /**
+   *
+   * path of the file to check the keys against when safe option is set to true.
+   * Defaults to .env.sample
+   *
+   */
 
-	sampleFilePath?: string;
-	
-	/**
-	 *
-	 * whether to expand the variables or not 
-	 * Defaults to false
-	 *
-	 */
-	
-	
-	expand?: boolean;
+  sampleFilePath?: string
 
-	/**
-	 * checks whether the used env file is missing some keys.
-	 *
-	 * For example, if the given `.env` file has the following content:
-	 * ```
-	 * VAR=true
-	 * ```
-	 *
-	 * and the `.env.sample` file has the following:
-	 * ```
-	 * VAR=true
-	 * VAR2=sample value
-	 * ```
-	 *
-	 * the following error log will be printed:
-	 * ```
-	 * MissingEnvVarsError: [VAR2] were defined in .env.example but are not present in the environment:
-	 *     This may cause the app to misbehave.
-	 * ```
-	 */
+  /**
+   *
+   * whether to expand the variables or not
+   * Defaults to false
+   *
+   */
 
-	safe?: boolean;
+  expand?: boolean
 
-	/**
-	 * This turns on parse logs which help debug how keys are being parsed.
-	 *
-	 * default : false
-	 */
+  /**
+   * checks whether the used env file is missing some keys.
+   *
+   * For example, if the given `.env` file has the following content:
+   * ```
+   * VAR=true
+   * ```
+   *
+   * and the `.env.sample` file has the following:
+   * ```
+   * VAR=true
+   * VAR2=sample value
+   * ```
+   *
+   * the following error log will be printed:
+   * ```
+   * MissingEnvVarsError: [VAR2] were defined in .env.example but are not present in the environment:
+   *     This may cause the app to misbehave.
+   * ```
+   */
 
-	parseLog?: boolean;
+  safe?: boolean
 
-	/**
-	 *
-	 * This option allows you to pass in a pre-defined logger instance.
-	 * The logger must implement the NestJS LoggerService interface
-	 *
-	 */
+  /**
+   * This turns on parse logs which help debug how keys are being parsed.
+   *
+   * default : false
+   */
 
-	logger?: LoggerService;
+  parseLog?: boolean
 
-	/**
-	 *
-	 * This option allows you assign the values to process.env . Defaults to true
-	 *
-	 */
+  /**
+   *
+   * This option allows you to pass in a pre-defined logger instance.
+   * The logger must implement the NestJS LoggerService interface
+   *
+   */
 
-	assignToProcessEnv?: boolean;
+  logger?: LoggerService
 
-	/**
-	 *
-	 * This option allows you overide a value on process.env if its already set . Defaults to false
-	 *
-	 */
+  /**
+   *
+   * This option allows you assign the values to process.env . Defaults to true
+   *
+   */
 
-	overrideProcessEnv?: boolean;
+  assignToProcessEnv?: boolean
+
+  /**
+   *
+   * This option allows you overide a value on process.env if its already set . Defaults to false
+   *
+   */
+
+  overrideProcessEnv?: boolean
 }
--- src/easyconfig.module.ts
+++ src/easyconfig.module.ts
@@ -1,20 +1,21 @@
-import { Module, DynamicModule, Global } from '@nestjs/common';
-import { EasyconfigService } from './easyconfig.service';
-import { Config } from './config.interface';
+import type { DynamicModule } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
+import { EasyconfigService } from "./easyconfig.service";
+import type { Config } from "./config.interface";
 
 @Global()
 @Module({})
 export class EasyconfigModule {
-	static register(options?: Config): DynamicModule {
-		return {
-			module: EasyconfigModule,
-			providers: [
-				{
-					provide: EasyconfigService,
-					useValue: new EasyconfigService(options),
-				},
-			],
-			exports: [EasyconfigService],
-		};
-	}
+  static register(options?: Config): DynamicModule {
+    return {
+      module: EasyconfigModule,
+      providers: [
+        {
+          provide: EasyconfigService,
+          useValue: new EasyconfigService(options),
+        },
+      ],
+      exports: [EasyconfigService],
+    };
+  }
 }
